762443b6d5ebedd23da98910620a0c5d86fddcc8
João Victor Zignago <98095084+Zigsnago@users.noreply.github.com>
2022-05-08 15:52:50 -0300

    RaizV2

--- public/js/calc_functions.js
+++ public/js/calc_functions.js
@@ -1,69 +1,93 @@
-function atualizarDisplay(btn){
+function atualizarDisplay(btn) {
     const display = document.getElementById('display');
-    if(display.value.length === 9) return;
-    if(display.value === '0') display.value = btn.value;
+    if (display.value.length === 11) return;
+    if (display.value === '0') display.value = btn.value;
     else display.value += btn.value;
 }
 
-function limparDisplay(){
+function limparDisplay() {
     document.getElementById('display').value = '0';
 }
 
 var operador = ''
-var valor1 
-function atualizarOperacao(btn){
+var valor1
+function atualizarOperacao(btn) {
     const display = document.getElementById('display');
-    if(btn.value == "( )"){
+    if (btn.value == "( )") {
         display.value = addDentro(display.value)
     }
-    if(btn.value == "√"){
-        display.value = "√"
-     }
-    else{
+    if (btn.value == "√") {
+        if (display.value == "0") {
+            display.value = "√"
+        } else {
+            display.value += "√"
+        }
+    }
+    else {
         operador = btn.value;
         valor1 = display.value;
         display.value += btn.value;
     }
-    
+
 }
 
-function addDentro(valor){
-    return "("+valor+")"
+function addDentro(valor) {
+    return "(" + valor + ")"
 }
 
-function calcularOperacao(){
+function calcularOperacao() {
     const display = document.getElementById('display');
     let valor2 = display.value;
-    if(valor2.includes("√")){
-        let resultado = Math.sqrt(valor2.substr(valor2.indexOf("√")+1, verificaOperaçao(valor2)-1)).toFixed(2)
-        var aposSinal = ""
-        if(verificaOperaçao(valor2) != valor2.length){
-            aposSinal = valor2.substr(verificaOperaçao(valor2), valor2.length)
-            console.log(aposSinal)
-        }
-        valor2 = resultado+aposSinal
-        console.log(valor2)
-    }
+    valor2 = VerificaRaiz(valor2)
+
     valor1 = eval(valor2);
     display.value = valor1;
     operador = '';
-}       
-
-function manipularTeclado(){
-    if(/[0-9]/.test(event.key))
-        atualizarDisplay({value: event.key});
 }
 
-function verificaOperaçao(string){
-    if(string.includes("+") == true){
-        return string.indexOf("+")
-    }else if(string.includes("-") == true){
-        return string.indexOf("-")
-    }else if(string.includes("*") == true){
-        return string.indexOf("*")
-    }else if(string.includes("/") == true){
-        return string.indexOf("/")
-    }else{
-        return string.length;
+function manipularTeclado() {
+    if (/[0-9]/.test(event.key))
+        atualizarDisplay({ value: event.key });
+}
+
+function VerificaRaiz(string) {
+    let newString = string
+    while (newString.includes("√")) {
+        var indiceRaiz = string.indexOf("√")
+        if (verificaOperaçao(string) > indiceRaiz) {
+
+            newString = string.replace(string.substr(indiceRaiz, verificaOperaçao(string)), Math.sqrt(string.substr(indiceRaiz + 1, verificaOperaçao(string) - 1)))
+
+        } else {
+
+            newString = string.replace(string.substr(indiceRaiz, string.length - 1), Math.sqrt(string.substr(indiceRaiz + 1, string.length - 1)))
+
+        }
     }
+    return newString
+}
+
+
+
+
+
+function verificaOperaçao(string) {
+    if (string.includes("+") || string.includes("-") || string.includes("*") || string.includes("/")) {
+        var indicemenor = 10000
+        if (string.includes("-") == true && string.indexOf("-") < indicemenor) {
+            indicemenor = string.indexOf("-")
+        }
+        if (string.includes("*") == true && string.indexOf("*") < indicemenor) {
+            indicemenor = string.indexOf("*")
+        }
+        if (string.includes("/") == true && string.indexOf("/") < indicemenor) {
+            indicemenor = string.indexOf("/")
+        }
+        if (string.includes("+") == true && string.indexOf("+") < indicemenor) {
+            indicemenor = string.indexOf("+")
+        }
+    } else {
+        indicemenor = string.length
+    }
+    return indicemenor;
 }
